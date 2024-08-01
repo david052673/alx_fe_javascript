b5d18ee0ff9ba050f0b02a94238eef533713822a
David <davidmwathi412@gmail.com>
2024-08-01 15:52:39 +0300

    Added changes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -42,3 +42,17 @@ function  createAddQuoteForm() {
  newQuoteText.appendChild(newQuote)
     
 );
+
+
+
+
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
