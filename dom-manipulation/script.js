faae9a52e01293d456b1272b50b04f55b4835753
David <davidmwathi412@gmail.com>
2024-08-01 14:38:36 +0300

    Added changes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,3 +1,40 @@
-function name() {
+const quotes =[
+    {text:"EVERYTHING IS GREAT",category:"Advice",
+},
+{
+    text:"Golden Hit", category:"book",
+},
+{
+    text:"THE FORCE", category:"movie"
+}
+
+
+
+];
+
+
+function displayRandomqoute() {
+    const qoute = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    console.log(`"${randomQuote.text}" - ${randomQuote.category}`);
+
+displayRandomqoute();
+};
+function displayRandomQuote () {
+    const newQuoteText = document.getElementById("newQuoteText").value;
+    const newQuoteCategory = document.getElementById("newQuoteCategory").value;
+  
+    
+    const newQuote = {
+      text: newQuoteText,
+      category: newQuoteCategory,
+    };
+  
+    
+    quotes.push(newQuote);
+
+    displayRandomQuote();
+  }
+
     
 }
